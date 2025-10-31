ae2f17d0c75164500bc3aa1352e7f85ebb87ed75
Altimetrikjiraaccesstest <altimetrikjiraaccess@gmail.com>
2025-10-31 19:31:36 +0530

    Add import for Selenium Keys in SalesforceLoginPage

--- src/main/java/com/example/framework/pages/SalesforceLoginPage.java
+++ src/main/java/com/example/framework/pages/SalesforceLoginPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.Keys;
 
 public class SalesforceLoginPage extends BasePage {
 
